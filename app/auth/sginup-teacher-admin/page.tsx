c94d5420af377385070e952e40ce23f1bf29a6a2
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2026-07-16 00:19:02 +0100

    ajustes no perfil

--- app/auth/sginup-teacher-admin/page.tsx
+++ app/auth/sginup-teacher-admin/page.tsx
@@ -1,9 +1,14 @@
 // app/admin/create-user/page.tsx
 import InternalRegisterForm from "@/components/auth/InternalRegisterForm";
+import { BackButton } from "@/components/shared/BackButton";
 
 export default function CreateInternalUserPage() {
     return (
-        <div className="min-h-screen bg-background flex items-center justify-center p-4">
+        
+        <div className="min-h-screen bg-background flex  flex-col items-center justify-center p-4">
+            <div className="flex justify-start">
+                    <BackButton destiny="/" />
+                  </div>
             <InternalRegisterForm />
         </div>
     );
